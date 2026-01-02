e94d47e57c535b802c1275ac11c355b889e0ea96
yeon-jae <lisanamyj99@gmail.com>
2026-01-02 16:27:41 +0900

    style topiccard & tags bg ground and change tag text to button

--- src/components/TopicCard.tsx
+++ src/components/TopicCard.tsx
@@ -7,19 +7,19 @@ interface Props {
 
 export default function TopicCard({ topic }: Props) {
   return (
-    <div className="bg-btn-sub w-[385x] h-[300px] p-8 mt-6 rounded shadow transition-all duration-300 hover:shadow-xl hover:-translate-y-2">
+    <div className="w-[385x] h-[300px] p-8 mt-6 rounded shadow transition-all duration-300 hover:shadow-xl hover:-translate-y-2 bg-gradient-to-br from-btn-sub-purple2 to-btn-sub hover:from-btn-sub hover:to-btn-sub-purple2">
       {/* 
       <p className="text-gray-700 mb-4">{topic.summary}</p> */}
 
       {/* 태그 */}
       <div className="mb-3 text-sm flex flex-wrap justify-center gap-2">
         {topic.tags?.map((tag) => (
-          <span
+          <button
             key={tag}
-            className="text-base inline-block bg-btn-sub-purple2 text-text-primary px-4 py-2 rounded-full"
+            className="text-base inline-block bg-[#3A5BA0] text-text-primary px-4 py-2 rounded-full shadow-md"
           >
             # {tag}
-          </span>
+          </button>
         ))}
       </div>
             <Link to={`/topics/${topic.id}`}>
