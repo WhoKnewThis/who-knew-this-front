fbb5be6e6c25637b8a79a9d6803a7894d122b613
yeon-jae <lisanamyj99@gmail.com>
2026-01-06 17:09:50 +0900

    feat google login btn

--- src/components/LoginBtn.tsx
+++ src/components/LoginBtn.tsx
@@ -2,12 +2,25 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function LoginBtn(){
-    const navigate= useNavigate();
+    const GOOGLE_AUTH_URL= "https://accounts.google.com/o/oauth2/v2/auth?" + 
+    new URLSearchParams({
+        client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+        redirect_uri: "http://localhost:8000/auth/callback", 
+        response_type: "code",
+      scope: "email profile",
+    }).toString();
+
+    const handleGoogleLogin=()=>{
+        window.location.href = GOOGLE_AUTH_URL;
+
+    }
 
     return(
-        <button
-            onClick={()=>navigate('/login')}
-            className="bg-btn-primary text-lg text-bg-primary p-2 w-[120px] h-12 rounded-md hover:bg-btn-primary2">Log in
-        </button>
+         <button
+      onClick={handleGoogleLogin}
+      className="bg-btn-primary text-lg text-bg-primary p-2 w-[120px] h-12 rounded-md hover:bg-btn-primary2"
+    >
+      Log in
+    </button>
     );
 }
