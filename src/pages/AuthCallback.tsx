c2a1abfe5ddb1f1be941a4e81307290251e392f9
yeon-jae <lisanamyj99@gmail.com>
2026-01-12 15:53:16 +0900

    feat authCallback.tsx page

--- src/pages/AuthCallback.tsx
+++ src/pages/AuthCallback.tsx
@@ -2,9 +2,15 @@ import axios from "axios";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
-
 export default function AuthCallback(){
+    const navigate= useNavigate();
+
+
     return(
-        <div>authcallback page</div>
+        <div className="p-10">
+            <h2 className="text-lg font-semibold">
+                로그인 처리 중 ...
+            </h2>
+        </div>
     );
 }
